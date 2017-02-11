7ef9711f5fed40facefbb1e0d83f9f24d8c67eb9
PornchaiL <cutie_chai@hotmail.com>
2017-02-12 00:50:39 +0700

    set schedule run every 10 minutes to fetch new data

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 var config = require('./config');
 var apiRouter = require('./api');
 var bodyParser = require('body-parser');
+var dataProvider = require('./dbmodel/dataprovider');
 var express = require('express');
 var server = express();
+var axios = require('axios');
+var schedule = require('node-schedule');
+var taskSchedule = new schedule.RecurrenceRule();
+taskSchedule.minute = 10;
 
 server.set('view engine', 'ejs');
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+function scrapeOnSchdeule () {
+	dataProvider.getStockList(function(data){		
+		// no snapshot data in db
+		if (data != undefined && data.length === 0) {
+			axios.get(config.serverUrl()+'/api/scrapestocklist')
+				.then(function(response){
+					if (response.data.length > 0) {
+						dataProvider.insertStockList(response.data);
+					}
+			});
+		} else {
+			axios.get(config.serverUrl()+'/api/scrapestocklist')
+				.then(function(response){
+					if (response.data.length > 0) {
+						dataProvider.updateStockList(response.data);
+					}
+				});
+		}
+	});
+}
+
+schedule.scheduleJob(taskSchedule, scrapeOnSchdeule);
 
 server.listen(config.port, config.host, function () {
   console.log('Example app listening on port: ' + config.port);
