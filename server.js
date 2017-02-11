8da743ad800b0b94f957138150af208181082982
PornchaiL <cutie_chai@hotmail.com>
2017-02-11 15:34:05 +0700

    attach api router

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var config = require('./config');
+var apiRouter = require('./api');
+var bodyParser = require('body-parser');
 var express = require('express');
 var server = express();
 
 server.set('view engine', 'ejs');
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
-server.use(express.static('public'));
-
-server.get('/', function (req, res) {
-	res.send({ status:'ok', data: []]});
-});
+server.use('/api', apiRouter);
 
 server.listen(config.port, config.host, function () {
   console.log('Example app listening on port: ' + config.port);
