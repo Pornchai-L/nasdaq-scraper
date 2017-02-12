16ac93bb48c150498890dcf92a4c640be99212fc
PornchaiL <cutie_chai@hotmail.com>
2017-02-12 11:53:39 +0700

    add more test in dbmodel#1

--- test/dbmodel/test-dataprovider.js
+++ test/dbmodel/test-dataprovider.js
@@ -1,15 +1,55 @@
 const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
-const dataprovider = require('../dbmodel/dataprovider');
+const dataprovider = require('../../dbmodel/dataprovider');
 
 describe('Test dataprovider module', function() {
-  describe('dataprovider insertStockList', function() {
-      it('shoud return ok if success', function() {
-        var dummy = {}
-        dataprovider.insertStockList(dummy, function(response) {
-            expect(response.status).to.equal('ok');
-        });
-      });
-  });
+	describe('dataprovider importDummyStockData', function() {
+		it('shoud return ok if success', function() {
+			var mockRes = function() {
+				this.value = "";
+				this.send = function(text) {
+					this.value = text;
+				}
+			} 
+			dataprovider.importDummyStockData(mockRes, function(response) {
+				expect(response.status).to.equal('ok');
+				expect(mockRes.value).to.equal('ok');
+			});
+		});
+	});
+
+	describe('dataprovider importDummySVGChart', function() {
+		it('shoud return ok if success', function() {
+			var mockRes = function() {
+				this.value = "";
+				this.send = function(text) {
+					this.value = text;
+				}
+			} 
+			dataprovider.importDummySVGChart(mockRes, function(response) {
+				expect(response.status).to.equal('ok');
+				expect(mockRes.value).to.equal('ok');
+			});
+		});
+	});
+
+	describe('dataprovider insertStockList', function() {
+		it('shoud return ok if success', function() {
+			var dummy = {}
+			dataprovider.insertStockList(dummy, function(response) {
+				expect(response.status).to.equal('ok');
+			});
+		});
+	});
+
+	describe('dataprovider dropStockAll', function() {
+		it('shoud return ok if success', function() {
+			dataprovider.dropStockAll(function(response) {
+				expect(response.status).to.equal('ok');
+			});
+		});
+	});
+
+	
 });
