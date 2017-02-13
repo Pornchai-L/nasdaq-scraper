5631a9af42fad95254f279c48ddf583c31dd0496
PornchaiL <cutie_chai@hotmail.com>
2017-02-13 23:16:46 +0700

    add http get/post test case

--- test/test-server.js
+++ test/test-server.js
@@ -1,22 +1,58 @@
 const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
-const dataprovider = require('../dbmodel/dataprovider');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
+chai.use(chaiHttp);
+// const dataprovider = require('../dbmodel/dataprovider');
 
 describe('test-server', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-      expect([1,2,3].indexOf(4)).to.equal(-1);
+  describe('/GET stocklist', function() {
+    it('should return 200 and array of data', function() {
+      chai.request(server)
+        .get('/stocklist')
+        .end(function(res){
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(0);
+          done();
+        });
     });
-  });
 
-  describe('dataprovider insertStockList', function() {
-      it('shoud return ok if success', function() {
-        var dummy = {}
-        dataprovider.insertStockList(dummy, function(response) {
-            expect(response.status).to.equal('ok');
+
+    describe('/POST stock data', function() {
+    it('should return 200 and chart data when input in range[0-6]', function() {
+      var input = {
+        "stockId": 1
+      };
+      chai.request(server)
+        .post('/stockchart')
+        .send(input)
+        .end(function(res){
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.length.should.be.eql(0);
+          done();
         });
       });
+    });
+
+    describe('/POST stock data', function() {
+    it('should return 404 and chart data when input out of range[0-6]', function() {
+      var input = {
+        "stockId": 7
+      };
+      chai.request(server)
+        .post('/stockchart')
+        .send(input)
+        .end(function(res){
+          res.should.have.status(404);
+          res.body.should.be.a('object');
+          res.body.length.should.be.eql(0);
+          done();
+        });
+      });
+    });
   });
 });
